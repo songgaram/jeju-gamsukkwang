2331b2c26d593f132a2048de8771b587c0b55621
Isacc <dewrain331@gmail.com>
2022-06-03 10:34:34 +0900

    chore : db/index.js 코드 수정

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -7,3 +7,5 @@ const db = mongoose.connection;
 
 db.on("connected", () => console.log(`MongoDB 연결 성공 : ${DB_URL}`));
 db.on("error", (err) => console.error(`MongoDB 연결 실패 : ${DB_URL}, err`));
+
+export { db };
