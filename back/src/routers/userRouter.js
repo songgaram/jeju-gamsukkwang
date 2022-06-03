e3cd0a531857ba62d5d167e3ebbbe8a7d9b0d0ff
Isacc <dewrain331@gmail.com>
2022-06-03 11:03:34 +0900

    feat : userService 회원가입 기능 추가
    - userService 회원가입 기능 추가 : addUser
    - jwt, uuid 패키지 설치
    
    #5

---
+++ back/src/routers/userRouter.js
@@ -0,0 +1,2 @@
+import { Router } from "express";
+import { userService } from "../services/userService";
---
+++ back/src/services/userService.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken";
+
+import { db, User } from "../db";
+
+class userService {
+	// 회원 등록 기능
+	static addUser = async ({ email, password, nickname }) => {
+		// 이메일 중복 확인
+		const user = await User.findByEmail({ email });
+		if (user) {
+			const errMessage = "이 이메일은 현재 사용중입니다.";
+			return { errMessage };
+		}
+
+		// 비밀번호 해쉬화
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// id에 유니크 값 부여
+		const id = uuidv4();
+		const newUser = { id, email, password: hashedPassword, nickname };
+
+		// db에 저장
+		const createdNewUser = await User.create({ newUser });
+		// 문제없이 db에 저장되었으므로 에러 메세지를 출력하지 않음.
+		createdNewUser.errorMessage = null;
+
+		return createdNewUser;
+	};
+}
