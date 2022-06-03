0e4b024644d051e8c145c5c76fb9ff01d98a2811
Isacc <dewrain331@gmail.com>
2022-06-03 11:29:39 +0900

    chore : db 연결 실패 시 코드 수정

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+import { User } from "./models/User";
+
 const DB_URL = process.env.MONGODB_URL || "MongoDB 서버 주소를 설정해주세요.";
 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
 db.on("connected", () => console.log(`MongoDB 연결 성공 : ${DB_URL}`));
-db.on("error", (err) => console.error(`MongoDB 연결 실패 : ${DB_URL}, err`));
+db.on("error", (err) =>
+	console.error(`MongoDB 연결 실패 : ${DB_URL}` + "\n" + err)
+);
 
 export { db };
+export { User };
