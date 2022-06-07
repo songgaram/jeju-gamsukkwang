8cd3f190ffa2ab4c0da4b1c264493f66bdfecfe9
songgaram <pager1436@gmail.com>
2022-06-07 16:47:45 +0900

    docs : image path 수정

--- front/src/page/intro/Intro.js
+++ front/src/page/intro/Intro.js
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+
 function Intro() {
-  return <div></div>;
+  return (
+    <IntroContainer>
+      <Header />
+    </IntroContainer>
+  );
 }
+
+const IntroContainer = styled.div`
+  width: 100vw;
+  height: auto;
+  background-color: ivory;
+`;
+
+const Header = styled.div`
+  height: 80px;
+`;
+// const BackgroundImage = styled.div`
+//   background: url("/assets/images/IntroBackground.png");
+// `;
 
 export default Intro;
