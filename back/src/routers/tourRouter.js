2b5ec33c29dfbcc8f767c5acf7f35f022d8ccd14
Isacc <dewrain331@gmail.com>
2022-06-08 15:56:36 +0900

    feat : Tour MVP 좋아요 기능 구현중
    - 해당 랜드마크 좋아요 추가 확인
    - 해당 랜드마크 좋아요 삭제 확인
    - 중복 확인 후 기능 prohibit 추가 필요
    
    #22

--- back/src/routers/tourRouter.js
+++ back/src/routers/tourRouter.js
@@ -23,4 +23,48 @@ tourRouter.get("/tour", async (req, res, next) => {
 	}
 });
 
+// 랜드마크 좋아요 추가
+tourRouter.put("/tour/like", loginRequired, async (req, res, next) => {
+	try {
+		if (is.emptyObject(req.query)) {
+			throw new Error("system.error.noTitle");
+		}
+
+		// req에서 데이터 가져오기
+		const userId = req.currentUserId;
+		const { name } = req.query;
+
+		const addLiketoLandmark = await tourService.addLike({
+			name,
+			currentUserId: userId,
+		});
+
+		res.status(200).json(addLiketoLandmark);
+	} catch (err) {
+		next(err);
+	}
+});
+
+// 랜드마크 싫어요 추가
+tourRouter.put("/tour/dislike", loginRequired, async (req, res, next) => {
+	try {
+		if (is.emptyObject(req.query)) {
+			throw new Error("system.error.noTitle");
+		}
+
+		// req에서 데이터 가져오기
+		const userId = req.currentUserId;
+		const { name } = req.query;
+
+		const removeLikefromLandmark = await tourService.removeLike({
+			name,
+			currentUserId: userId,
+		});
+
+		res.status(200).json(removeLikefromLandmark);
+	} catch (err) {
+		next(err);
+	}
+});
+
 export { tourRouter };
--- back/src/services/tourService.js
+++ back/src/services/tourService.js
@@ -11,6 +11,34 @@ class tourService {
 
 		return landmark;
 	};
+
+	static addLike = async ({ name, currentUserId }) => {
+		const isLandmarkExist = await tourModel.isLandmarkExist({ enTitle: name });
+		if (!isLandmarkExist) {
+			throw new Error("system.error.noLandmark");
+		}
+
+		const addLiketoLandmark = tourModel.addLike({
+			enTitle: name,
+			currentUserId,
+		});
+
+		return addLiketoLandmark;
+	};
+
+	static removeLike = async ({ name, currentUserId }) => {
+		const isLandmarkExist = await tourModel.isLandmarkExist({ enTitle: name });
+		if (!isLandmarkExist) {
+			throw new Error("system.error.noLandmark");
+		}
+
+		const removeLikefromLandmark = tourModel.removeLike({
+			enTitle: name,
+			currentUserId,
+		});
+
+		return removeLikefromLandmark;
+	};
 }
 
 export { tourService };
