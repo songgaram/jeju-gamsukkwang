42c0a599b88c5ee47f814755ab359468261fb28d
Isacc <dewrain331@gmail.com>
2022-06-11 22:03:23 +0900

    fix : reviewModel 수정
     - update userId 관련 코드 수정

--- back/src/db/models/reviewModel.js
+++ back/src/db/models/reviewModel.js
@@ -1,46 +1,45 @@
-import { Review } from '../schemas/review.js';
+import { Review } from "../schemas/review.js";
 
 export const reviewModel = {
-  // 리뷰 작성하기
-  create: async ({ newReview }) => {
-    const createdNewReview = await Review.create(newReview)
-    return createdNewReview
-  },
+	// 리뷰 작성하기
+	create: async ({ newReview }) => {
+		const createdNewReview = await Review.create(newReview);
+		return createdNewReview;
+	},
 
-  // 해당 랜드마크에 대한 리뷰를 이미 작성했던 사람인지 확인
-  IsPosted: async ({ landmarkId, writerId }) => {
-    const IsPosted = Review.exists({ 
-      $and: [{ landmarkId }, { writerId } ]
-    })
-    return IsPosted
-  },
+	// 해당 랜드마크에 대한 리뷰를 이미 작성했던 사람인지 확인
+	IsPosted: async ({ landmarkId, writerId }) => {
+		const IsPosted = Review.exists({
+			$and: [{ landmarkId }, { writerId }],
+		});
+		return IsPosted;
+	},
 
-  // 해당 랜드마크의 리뷰 목록 최신순으로 불러오기
-  findByLandmarkId: async ({ landmarkId }) => {
-    const reviews = await Review.find({ landmarkId }).sort({ createdAt: -1 })
-    return reviews
-  },
+	// 해당 랜드마크의 리뷰 목록 최신순으로 불러오기
+	findByLandmarkId: async ({ landmarkId }) => {
+		const reviews = await Review.find({ landmarkId }).sort({ createdAt: -1 });
+		return reviews;
+	},
 
-  // reviewId로 리뷰 정보 찾기
-  findById: async ({ reviewId }) => {
-    const review = await Review.findOne({ reviewId })
-    return review
-  },
+	// reviewId로 리뷰 정보 찾기
+	findById: async ({ reviewId }) => {
+		const review = await Review.findOne({ id: reviewId });
+		return review;
+	},
 
-  // 리뷰 수정하기
-  update: async ({ reviewId, data }) => {
-    const filter = { id: reviewId}
+	// 리뷰 수정하기
+	update: async ({ reviewId, data }) => {
+		const filter = { id: reviewId };
 		const update = { $set: data };
 		const option = { returnOriginal: false };
 
 		const updatedReview = await Review.findOneAndUpdate(filter, update, option);
 		return updatedReview;
-  },
+	},
 
-  // 리뷰 삭제하기
-  deleteById: async ({ reviewId }) => {
-    const deleteResult = await Review.deleteOne({ id: reviewId })
-    return deleteResult
-  }
-
+	// 리뷰 삭제하기
+	deleteById: async ({ reviewId }) => {
+		const deleteResult = await Review.deleteOne({ id: reviewId });
+		return deleteResult;
+	},
 };
