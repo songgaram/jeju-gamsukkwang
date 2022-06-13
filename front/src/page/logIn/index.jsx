0fd8bfb87bf79564569f3da658429287632ae678
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-06-13 14:39:49 +0900

    feat: 로그인/회원가입 실패 모달 추가

--- front/src/page/logIn/index.jsx
+++ front/src/page/logIn/index.jsx
@@ -3,9 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 import http from "libs/apiController";
 import logInValidation from "./utils";
-import { LOGIN_INIT_DATA, ERROR_MESSAGE } from "./constants";
-
+import { LOGIN_INIT_DATA, ERROR_MESSAGE, MODAL_MESSAGE } from "./constants";
 import Input from "components/input";
+import Modal from "components/modal";
+import ModalPortal from "components/modal/modalPortal";
+
 import LogoIcon from "assets/images/LogoIcon.png";
 import {
   LogInContainer,
@@ -16,9 +18,11 @@ import {
 } from "./logIn.style";
 
 const LogIn = () => {
-  const navigate = useNavigate();
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const [userInputData, setUserInputData] = useState(LOGIN_INIT_DATA);
   const { email, password } = userInputData;
+
+  const navigate = useNavigate();
 
   const { isEmailValid, isPasswordValid } = logInValidation(userInputData);
 
@@ -49,53 +53,58 @@ const LogIn = () => {
       localStorage.setItem("accessToken", res.data.token);
       navigate("/");
     } catch (err) {
-      console.log("로그인에 실패하였습니다.\n", err);
-      alert(
-        "🤯 로그인에 실패하였습니다 🤯 \n 아이디와 비밀번호를 확인해주세요!",
-      );
+      setIsOpenModal(true);
+      navigate("/login");
     }
   };
 
   return (
-    <LogInContainer>
-      <img src={LogoIcon} alt="제주감수꽝 마스코트 이미지" />
-      <Title>로그인</Title>
-      <LogInForm>
-        <InputBox>
-          <label htmlFor="email">이메일</label>
-          <Input
-            type="email"
-            name="email"
-            placeholder="이메일 주소를 입력해주세요."
-            onChange={handleOnChange}
-            required
-          />
-          {isInValid.email && <p>{ERROR_MESSAGE.email}</p>}
-        </InputBox>
-        <InputBox>
-          <label htmlFor="password">비밀번호</label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="비밀번호를 입력해주세요."
-            onChange={handleOnChange}
-            required
-          />
-          {isInValid.password && <p>{ERROR_MESSAGE.password}</p>}
-        </InputBox>
-        <LoginButton
-          type="submit"
-          onClick={handleOnSubmit}
-          disabled={!isActive}
-          isActive={isActive}
-        >
-          로그인
-        </LoginButton>
-        <span type="button" onClick={() => navigate("/register")}>
-          제주감수꽝 회원 가입하기 〉
-        </span>
-      </LogInForm>
-    </LogInContainer>
+    <>
+      <LogInContainer>
+        <img src={LogoIcon} alt="제주감수꽝 마스코트 이미지" />
+        <Title>로그인</Title>
+        <LogInForm>
+          <InputBox>
+            <label htmlFor="email">이메일</label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="이메일 주소를 입력해주세요."
+              onChange={handleOnChange}
+              required
+            />
+            {isInValid.email && <p>{ERROR_MESSAGE.email}</p>}
+          </InputBox>
+          <InputBox>
+            <label htmlFor="password">비밀번호</label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="비밀번호를 입력해주세요."
+              onChange={handleOnChange}
+              required
+            />
+            {isInValid.password && <p>{ERROR_MESSAGE.password}</p>}
+          </InputBox>
+          <LoginButton
+            type="submit"
+            onClick={handleOnSubmit}
+            disabled={!isActive}
+            isActive={isActive}
+          >
+            로그인
+          </LoginButton>
+          <span type="button" onClick={() => navigate("/register")}>
+            제주감수꽝 회원 가입하기 〉
+          </span>
+        </LogInForm>
+      </LogInContainer>
+      <ModalPortal>
+        {isOpenModal && (
+          <Modal setIsOpenModal={setIsOpenModal} modalMessage={MODAL_MESSAGE} />
+        )}
+      </ModalPortal>
+    </>
   );
 };
 
