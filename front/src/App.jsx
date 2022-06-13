4a85ba6095b9d52b4442c466597ba8ebd166a83c
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-06-13 16:43:58 +0900

    feat: header 기능 구현

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
+import Layout from "components/layout";
 import Home from "page/home";
 import Register from "page/register";
 import Intro from "page/intro";
@@ -8,10 +9,12 @@ import LogIn from "page/logIn";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/intro" element={<Intro />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/intro" element={<Intro />} />
+      </Route>
     </Routes>
   );
 };
--- front/src/assets/svgs/index.js
+++ front/src/assets/svgs/index.js
@@ -1,1 +1,2 @@
 export { ReactComponent as ExclamationIcon } from "./exclamationIcon.svg";
+export { ReactComponent as Logo } from "./logo.svg";
