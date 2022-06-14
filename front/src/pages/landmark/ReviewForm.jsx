ded9e4c341704076a2c4ede227c9cfa37b25eb1b
songgaram <pager1436@gmail.com>
2022-06-14 15:40:36 +0900

    feat : header code 추가

--- front/src/pages/landmark/ReviewForm.jsx
+++ front/src/pages/landmark/ReviewForm.jsx
@@ -8,29 +8,60 @@ const ReviewForm = () => {
   const [clicked, setClicked] = useState(null);
 
   return (
-    <StarContainer>
-      {[1, 2, 3, 4, 5].map((el) => (
-        <BsStarFill
-          color={
-            (clicked >= el) | (hovered >= el)
-              ? theme.colors.secondary
-              : theme.colors.gray02
-          }
-          key={el}
-          size={theme.fontSizes.titleSize}
-          onMouseEnter={() => setHovered(el)}
-          onMouseLeave={() => setHovered(null)}
-          onClick={() => setClicked(el)}
-        />
-      ))}
-    </StarContainer>
+    <>
+      <HeaderContainer align="center" justify="center">
+        <Title>랜드마크를 평가해주세요!</Title>
+        <Required>필수</Required>
+      </HeaderContainer>
+
+      <StarContainer>
+        {[1, 2, 3, 4, 5].map((el) => (
+          <BsStarFill
+            color={
+              (clicked >= el) | (hovered >= el)
+                ? theme.colors.secondary
+                : theme.colors.gray02
+            }
+            key={el}
+            size={theme.fontSizes.titleSize}
+            onMouseEnter={() => setHovered(el)}
+            onMouseLeave={() => setHovered(null)}
+            onClick={() => setClicked(el)}
+          />
+        ))}
+      </StarContainer>
+
+      <HeaderContainer align="center">
+        <Title>다른 여행객을 위한 후기와 팁</Title>
+        <Required>필수</Required>
+      </HeaderContainer>
+    </>
   );
 };
 
 const StarContainer = styled.div`
   text-align: center;
   border: none;
-  padding: 5% 1%;
+  padding: 3% 1%;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align};
+  margin-top: 5%;
+`;
+
+const Title = styled.div`
+  font-size: ${theme.fontSizes.xxxl};
+  font-weight: bold;
+  margin-right: 0.5%;
+`;
+
+const Required = styled.p`
+  font-size: ${theme.fontSizes.base};
+  color: ${theme.colors.secondary};
+  font-weight: bold;
 `;
 
 export default ReviewForm;
