bb83930a84eb317773bc3227ea05d251676bfaba
보현 김 <nhs04047@gmail.com>
2022-06-15 23:40:07 +0900

    feat : flask 서버 test

---
+++ ai/back/app.py
@@ -0,0 +1,24 @@
+from distutils.log import debug
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+import prediction
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/prediction', methods=['POST'])
+def post():
+  if request.method == 'POST':
+    res = request.get_json()
+    imgURL = res['imgURL']
+    print(imgURL)
+    prediction.predictImage(imgURL)
+    
+
+if __name__ == '__main__':
+  app.run(port=5001, debug=True)
