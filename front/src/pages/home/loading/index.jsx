24d74b3d5f357a93aaf61785bce7695dc633c79b
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-06-29 20:01:50 +0900

    chore: loading 스타일 분리

--- front/src/pages/home/loading/index.jsx
+++ front/src/pages/home/loading/index.jsx
@@ -1,7 +1,7 @@
 import ModalPortal from "components/modal/modalPortal";
 import { LogoIcon } from "assets/svgs";
 
-import styled, { keyframes } from "styled-components";
+import { ModalBackground, ModalBox, LogoBox } from "./loading.style";
 
 const Loading = () => {
   return (
@@ -19,67 +19,3 @@ const Loading = () => {
 };
 
 export default Loading;
-
-const fadeIn = keyframes`
- from {
-    background: rgba(0, 0, 0, 0%);
-  }
-
-  to {
-    background: rgba(0, 0, 0, 30%);
-    backdrop-filter: blur(5px);
-  }
-`;
-
-const ModalBackground = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100vh;
-  animation: ${fadeIn} 0.4s cubic-bezier(0.5, 0, 0, 0.8) forwards;
-
-  * {
-    font-weight: 600;
-    color: ${({ theme }) => theme.colors.black};
-  }
-`;
-
-const slideUp = keyframes`
-  from {
-    opacity: 0;
-    transform: scale(0.8) translateY(100%);
-  }
-
-  to {
-    opacity: 1;
-    transform: scale(1) translateY(0);
-  }
-`;
-
-const ModalBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 400px;
-  height: 300px;
-  padding: 50px;
-  background: ${({ theme }) => theme.colors.white};
-  border-radius: 20px;
-  animation: ${slideUp} 0.4s cubic-bezier(0.5, 0, 0, 0.8) forwards;
-`;
-
-const rotateImage = keyframes`
-  100% {
-        transform: rotate(360deg);
-    }
-`;
-
-const LogoBox = styled.div`
-  animation: ${rotateImage} 1.8s linear infinite;
-  transform-origin: 50% 50%;
-`;
---
+++ front/src/pages/home/loading/loading.style.js
@@ -0,0 +1,67 @@
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+ from {
+    background: rgba(0, 0, 0, 0%);
+  }
+
+  to {
+    background: rgba(0, 0, 0, 30%);
+    backdrop-filter: blur(5px);
+  }
+`;
+
+const ModalBackground = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  animation: ${fadeIn} 0.4s cubic-bezier(0.5, 0, 0, 0.8) forwards;
+
+  * {
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.black};
+  }
+`;
+
+const slideUp = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.8) translateY(100%);
+  }
+
+  to {
+    opacity: 1;
+    transform: scale(1) translateY(0);
+  }
+`;
+
+const ModalBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 400px;
+  height: 300px;
+  padding: 50px;
+  background: ${({ theme }) => theme.colors.white};
+  border-radius: 20px;
+  animation: ${slideUp} 0.4s cubic-bezier(0.5, 0, 0, 0.8) forwards;
+`;
+
+const rotateImage = keyframes`
+  100% {
+        transform: rotate(360deg);
+    }
+`;
+
+const LogoBox = styled.div`
+  animation: ${rotateImage} 2.5s linear infinite;
+  transform-origin: 50% 50%;
+`;
+
+export { ModalBackground, ModalBox, LogoBox };
