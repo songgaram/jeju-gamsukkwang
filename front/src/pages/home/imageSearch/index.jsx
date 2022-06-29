5dfb137dfe6910bf4ca0fae5a6c1efae9fac0bd7
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-06-29 20:02:22 +0900

    feat: 데이터 loading 로직 구현

--- front/src/pages/home/imageSearch/index.jsx
+++ front/src/pages/home/imageSearch/index.jsx
@@ -4,18 +4,26 @@ import { useRef } from "react";
 import http from "libs/apiController";
 
 import { ImageUploadBox, Span } from "./imageSearch.style";
+import { useState } from "react";
+import Loading from "../loading";
 
 const ImageSearch = () => {
   const photoInput = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleUploadImage = useCallback((e) => {
+  const handleUploadImage = useCallback(async (e) => {
     if (!e.target.files) return;
 
     const formData = new FormData();
     formData.append("imgFile", e.target.files[0]);
 
     try {
-      http.post("tour/image", formData);
+      setIsLoading(true);
+      const res = await http.post("tour/image", formData);
+
+      // res.data.data.summary[0] 감귤이가 찾은 랜드마크 이름
+      console.log(res.data.data.summary[0].categoryName);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -33,6 +41,7 @@ const ImageSearch = () => {
         />
       </ImageUploadBox>
       <Span>이미지 검색 기능은 jpg 파일만 지원됩니다.</Span>
+      {isLoading && <Loading />}
     </>
   );
 };
