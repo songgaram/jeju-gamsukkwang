533e8c3317d4b94585d21a75f34729d919399207
Isacc <dewrain331@gmail.com>
2022-06-30 18:00:32 +0900

    feat : Header 미디어쿼리 적용

--- front/src/pages/home/home.style.js
+++ front/src/pages/home/home.style.js
@@ -31,6 +31,9 @@ const ContentsBox = styled.div`
   span:last-child {
     background: linear-gradient(#fff 50%, rgba(255, 136, 29, 60%) 50%);
   }
+
+  @media screen and ${({ theme }) => theme.breakPoint} {
+  } ;
 `;
 
 const Button = styled.button`
