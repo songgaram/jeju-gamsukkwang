0a6e6badb22284debf4dd93b24a0c1087dcfad94
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-06-30 14:20:00 +0900

    delete: suspense 제거, lazy로딩 제거

--- front/src/components/layout/index.jsx
+++ front/src/components/layout/index.jsx
@@ -1,21 +1,18 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 
 import Header from "components/header";
-import Loader from "components/loader";
 
 const Layout = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <div>
-        <header>
-          <Header />
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
-    </Suspense>
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
