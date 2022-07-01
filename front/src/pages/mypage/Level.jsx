0c59f77ce0685027ecb255a1fd6133dc09d04828
Isacc <dewrain331@gmail.com>
2022-07-01 13:35:41 +0900

    chore : Level 미디어쿼리 적용

--- front/src/pages/mypage/Level.jsx
+++ front/src/pages/mypage/Level.jsx
@@ -98,6 +98,9 @@ const LevelBox = styled.div`
   flex-direction: row;
   align-items: center;
   margin-bottom: 5%;
+  @media screen and ${({ theme }) => theme.breakPoint} {
+    flex-direction: column;
+  }
 `;
 
 const FigureBox = styled.div`
@@ -146,6 +149,7 @@ const Number = styled.div`
 
   @media screen and ${({ theme }) => theme.breakPoint} {
     margin-left: 1rem;
+    margin-bottom: 1rem;
     font-size: 1.5rem;
   }
 `;
