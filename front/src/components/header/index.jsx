d49d13258f556e44cbaf00258e1623c488efb4e3
songgaram <pager1436@gmail.com>
2022-07-02 00:51:47 +0900

    feat : 마이페이지 로그인 유저만 접근 가능하게

--- front/src/components/header/index.jsx
+++ front/src/components/header/index.jsx
@@ -9,18 +9,21 @@ import {
 } from "styled-dropdown-component";
 
 import Button from "components/button/Button";
-import { NAV_LIST } from "./constants";
+import { NAV_LIST, MODAL_MESSAGE } from "./constants";
 import { loginState } from "../../states";
 
 import { Logo } from "assets/svgs/index";
 import { HeaderContainer, HeaderWrapper, Nav } from "./header.style";
 import theme from "../../styles/Theme";
+import Modal from "components/modal";
+import ModalPortal from "components/modal/modalPortal";
 
 const Header = () => {
   const navigate = useNavigate();
 
   const [isLogin, setInLogin] = useRecoilState(loginState);
   const [hidden, setHidden] = useState(true);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   const mediaQuery = useMediaQuery({ query: theme.breakPoint });
 
@@ -34,39 +37,57 @@ const Header = () => {
   return (
     <>
       {!mediaQuery ? (
-        <HeaderContainer>
-          <HeaderWrapper>
-            <Logo type="button" onClick={() => navigate("/")} />
-            <Nav>
-              <ul>
-                {NAV_LIST.map((data) => (
-                  <li key={`nav-link-${data.id}`}>
-                    <NavLink
-                      to={`${data.path}`}
-                      style={({ isActive }) => ({
-                        textDecoration: isActive ? "underline" : "none",
-                        textDecorationColor: isActive ? "#333" : "none",
-                        textUnderlinePosition: isActive ? "under" : "none",
-                      })}
+        <>
+          <HeaderContainer>
+            <HeaderWrapper>
+              <Logo type="button" onClick={() => navigate("/")} />
+              <Nav>
+                <ul>
+                  {NAV_LIST.map((data) => (
+                    <li
+                      key={`nav-link-${data.id}`}
+                      onClick={() => {
+                        if (data.id === "4" && !isLogin) {
+                          setIsOpenModal(true);
+                          navigate("/login");
+                        }
+                      }}
                     >
-                      {data.text}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-              {!isLogin && (
-                <Button type="button" onClick={() => navigate("/login")}>
-                  로그인
-                </Button>
-              )}
-              {isLogin && (
-                <Button type="button" onClick={handleLogoutClick}>
-                  로그아웃
-                </Button>
-              )}
-            </Nav>
-          </HeaderWrapper>
-        </HeaderContainer>
+                      <NavLink
+                        to={`${data.path}`}
+                        style={({ isActive }) => ({
+                          textDecoration: isActive ? "underline" : "none",
+                          textDecorationColor: isActive ? "#333" : "none",
+                          textUnderlinePosition: isActive ? "under" : "none",
+                        })}
+                      >
+                        {data.text}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+                {!isLogin && (
+                  <Button type="button" onClick={() => navigate("/login")}>
+                    로그인
+                  </Button>
+                )}
+                {isLogin && (
+                  <Button type="button" onClick={handleLogoutClick}>
+                    로그아웃
+                  </Button>
+                )}
+              </Nav>
+            </HeaderWrapper>
+          </HeaderContainer>
+          <ModalPortal>
+            {isOpenModal && (
+              <Modal
+                setIsOpenModal={setIsOpenModal}
+                modalMessage={MODAL_MESSAGE}
+              />
+            )}
+          </ModalPortal>
+        </>
       ) : (
         <>
           <HeaderContainer>
@@ -78,6 +99,12 @@ const Header = () => {
                 <DropdownMenu hidden={hidden} toggle={() => setHidden(!hidden)}>
                   {NAV_LIST.map((data) => (
                     <DropdownItem
+                      onClick={() => {
+                        if (data.id === "4" && !isLogin) {
+                          setIsOpenModal(true);
+                          navigate("/login");
+                        }
+                      }}
                       key={`nav-link-${data.id}`}
                       style={{ margin: "15px 0" }}
                     >
@@ -109,6 +136,14 @@ const Header = () => {
               )}
             </HeaderWrapper>
           </HeaderContainer>
+          <ModalPortal>
+            {isOpenModal && (
+              <Modal
+                setIsOpenModal={setIsOpenModal}
+                modalMessage={MODAL_MESSAGE}
+              />
+            )}
+          </ModalPortal>
         </>
       )}
     </>
