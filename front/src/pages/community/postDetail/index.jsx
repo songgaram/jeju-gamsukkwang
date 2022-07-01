406be4ed371c77c5a836826d283f4d87e7e9b990
songgaram <pager1436@gmail.com>
2022-07-02 00:27:20 +0900

    refact : 주석 제거

--- front/src/pages/community/postDetail/index.jsx
+++ front/src/pages/community/postDetail/index.jsx
@@ -1,83 +1,83 @@
-// import { useGetPost } from "queries/communityQuery";
-// import { useParams } from "react-router-dom";
-// import ReactHtmlParser from "react-html-parser";
-// // import dayjs from "dayjs";
+import { useGetPost } from "queries/communityQuery";
+import { useParams } from "react-router-dom";
+import ReactHtmlParser from "react-html-parser";
+import dayjs from "dayjs";
 
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const PostDetail = () => {
-//   const params = useParams();
-//   const postId = params.id;
-//   const postItem = useGetPost(postId);
+const PostDetail = () => {
+  const params = useParams();
+  const postId = params.id;
+  const postItem = useGetPost(postId);
 
-//   return (
-//     <PostDetailBox>
-//       <ContentBox>
-//         <h2>
-//           <span>[</span>
-//           {postItem?.data?.head === "question"
-//             ? "질문"
-//             : postItem?.data?.head === "info"
-//             ? "정보"
-//             : "잡담"}
-//           <span>]</span> {postItem?.data?.title}
-//         </h2>
-//         <p>
-//           {postItem?.data?.userNickName} ㆍ{" "}
-//           {`${postItem?.data?.createdAt}`.split("T")[0]}
-//         </p>
-//         <hr />
-//         <div>{ReactHtmlParser(`${postItem?.data?.content}`)}</div>
-//       </ContentBox>
-//     </PostDetailBox>
-//   );
-// };
+  return (
+    <PostDetailBox>
+      <ContentBox>
+        <h2>
+          <span>[</span>
+          {postItem?.data?.head === "question"
+            ? "질문"
+            : postItem?.data?.head === "info"
+            ? "정보"
+            : "잡담"}
+          <span>]</span> {postItem?.data?.title}
+        </h2>
+        <p>
+          {postItem?.data?.userNickName} ㆍ{" "}
+          {`${postItem?.data?.createdAt}`.split("T")[0]}
+        </p>
+        <hr />
+        <div>{ReactHtmlParser(`${postItem?.data?.content}`)}</div>
+      </ContentBox>
+    </PostDetailBox>
+  );
+};
 
-// export default PostDetail;
+export default PostDetail;
 
-// const PostDetailBox = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   margin: 50px 0;
+const PostDetailBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 50px 0;
 
-//   @media screen and ${({ theme }) => theme.breakPoint} {
-//     width: 100%;
-//     margin: 20px 0;
-//   }
-// `;
+  @media screen and ${({ theme }) => theme.breakPoint} {
+    width: 100%;
+    margin: 20px 0;
+  }
+`;
 
-// const ContentBox = styled.div`
-//   width: 800px;
-//   margin: 0 auto;
-//   box-shadow: 0 0 12px rgba(0, 0, 0, 8%);
-//   padding: 50px;
+const ContentBox = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  box-shadow: 0 0 12px rgba(0, 0, 0, 8%);
+  padding: 50px;
 
-//   h2 {
-//     font-size: 20px;
-//     font-weight: 600;
-//   }
+  h2 {
+    font-size: 20px;
+    font-weight: 600;
+  }
 
-//   p {
-//     font-size: 14px;
-//     color: ${({ theme }) => theme.colors.gray03};
-//   }
+  p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.colors.gray03};
+  }
 
-//   hr {
-//     margin: 20px 0;
-//     border: 0.3px solid ${({ theme }) => theme.colors.gray02};
-//   }
+  hr {
+    margin: 20px 0;
+    border: 0.3px solid ${({ theme }) => theme.colors.gray02};
+  }
 
-//   div {
-//     margin: 50px 0;
-//   }
+  div {
+    margin: 50px 0;
+  }
 
-//   * {
-//     margin-bottom: 10px;
-//   }
+  * {
+    margin-bottom: 10px;
+  }
 
-//   @media screen and ${({ theme }) => theme.breakPoint} {
-//     width: 100%;
-//     padding: 20px;
-//   }
-// `;
+  @media screen and ${({ theme }) => theme.breakPoint} {
+    width: 100%;
+    padding: 20px;
+  }
+`;
