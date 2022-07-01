338ce0d5d7bc558e15bf899783609efb4c79aec6
songgaram <pager1436@gmail.com>
2022-07-01 11:26:43 +0900

    fix : nav 바 animation 적용

--- front/src/pages/mypage/Navs.jsx
+++ front/src/pages/mypage/Navs.jsx
@@ -1,14 +1,32 @@
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
+import { darken } from "polished";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { NAV_LIST } from "./constants";
 
 const Navs = () => {
+  const [isSelected, setIsSelected] = useState([true, false]);
+  const theme = useTheme();
+  const handleChangeBtnColor = (id) => {
+    const newArr = [false, false];
+    newArr[id] = true;
+    setIsSelected(newArr);
+  };
+
   return (
     <NavContainer>
       <Nav>
         <ul>
           {NAV_LIST.map((data) => (
-            <li key={`nav-link-${data.id}`}>
+            <li
+              key={`nav-link-${data.id}`}
+              onClick={() => handleChangeBtnColor(data.id)}
+              style={
+                isSelected[data.id]
+                  ? { backgroundColor: theme.colors.primary }
+                  : { backgroundColor: theme.colors.gray02 }
+              }
+            >
               <NavLink to={`${data.path}`} style={{ textDecoration: "none" }}>
                 {data.text}
               </NavLink>
@@ -25,21 +43,27 @@ const Nav = styled.nav`
 
   ul {
     display: flex;
+    align-items: flex-end;
+    width: 100%;
 
     li {
       margin-right: 30px;
       text-align: center;
-      width: 7rem;
-      background-color: ${({ theme }) => theme.colors.orange};
+      width: 9%;
       border-radius: 25px 25px 0 0;
-      height: 50px;
       padding-top: 20px;
+      height: 45px;
+      transition: all 0.2s ease-in-out;
 
-      a {
-        font-size: ${({ theme }) => theme.fontSizes.lg};
-        font-weight: 600;
-        color: ${({ theme }) => theme.colors.white};
+      &:hover {
+        background-color: ${({ theme }) => darken(0.1, theme.colors.primary)};
       }
+    }
+
+    a {
+      font-size: ${({ theme }) => theme.fontSizes.lg};
+      font-weight: 600;
+      color: ${({ theme }) => theme.colors.white};
     }
   }
 `;
