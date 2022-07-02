af99c419dd4820d08a34949a16820f37578c78c0
Isacc <dewrain331@gmail.com>
2022-07-02 12:25:37 +0900

    feat : https 적용

--- back/index.js
+++ back/index.js
@@ -1,9 +1,16 @@
 import "dotenv/config";
+import https from "https";
+import fs from "fs";
+
 import { app } from "./src/app";
 
 // process.env.SERVER_PORT는 string type. 앞에 +를 붙이면 number로 온다.
 const PORT = +process.env.SERVER_PORT || 5000;
 
-app.listen(PORT, () => {
+const option = {
+  key: fs.readFileSync("kdt-ai4-team08.elicecoding.com-key.pem"),
+  cert: fs.readFileSync("kdt-ai4-team08.elicecoding.com.pem"),
+};
+https.createServer(option, app).listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다. Port Number : ${PORT}`);
 });
